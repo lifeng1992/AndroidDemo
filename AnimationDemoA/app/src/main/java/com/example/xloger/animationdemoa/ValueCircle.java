067da427a76050f991a79e331ce494c3ef966802
lifeng1335 <lifeng1335@outlook.com>
2016-05-07 10:56:37 +0800

    Save

--- AnimationDemoA/app/src/main/java/com/example/xloger/animationdemoa/ValueCircle.java
+++ AnimationDemoA/app/src/main/java/com/example/xloger/animationdemoa/ValueCircle.java
@@ -15,14 +15,15 @@ import android.view.View;
 public class ValueCircle extends View {
     //
     private float rad = 0;
+    private float paintWidth = 40f;
     // 1.创建一个画笔
     private Paint mPaint = new Paint();
     private Path path = new Path();
     // 2.初始化画笔
     private void initPaint() {
-        mPaint.setColor(Color.BLUE);        //设置画笔颜色
-        mPaint.setStyle(Paint.Style.STROKE);  //设置画笔模式为填充
-        mPaint.setStrokeWidth(4f);          //设置画笔宽度为10px
+        mPaint.setColor(Color.BLUE);            //设置画笔颜色
+        mPaint.setStyle(Paint.Style.STROKE);    //设置画笔模式为填充
+        mPaint.setStrokeWidth(paintWidth);      //设置画笔宽度为10px
     }
 
     public ValueCircle(Context context) {
@@ -38,7 +39,8 @@ public class ValueCircle extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        RectF mRectF = new RectF(0, 0, canvas.getWidth(), canvas.getHeight());
+        RectF mRectF = new RectF(paintWidth/2, paintWidth/2,
+                canvas.getWidth()-paintWidth/2, canvas.getHeight()-paintWidth/2);
         path.moveTo(canvas.getWidth()/2,canvas.getHeight());
         path.arcTo(mRectF, 90, rad);
         canvas.drawPath(path, mPaint);
